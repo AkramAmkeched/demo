3e876ee8e650e965db2b2e55ff2b8c02772938a1
aamkeched <akram.amkeched@insbaixcamp.cat>
2025-04-04 14:04:14 +0200

    Afegeixo linia de VScode

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
 
         System.out.println("Afegim Segona Linia");
         System.out.println("Afegim Tercera Linia");
+
+        System.out.println("Afegim Una linia des de el visual studio code");
+
     }
 }
